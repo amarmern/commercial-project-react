78a9d50af1676b1b67ca8220c4fa63d19e007a87
Amrendra Kumar <amarmca51@gmail.com>
2019-12-29 00:28:49 +0530

    Added collectiion OverView pagre to sepearating the shop file and and shop data reducer

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,26 +1,12 @@
 import React from "react";
+import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
 
-import SHOP_DATA from "./shop.data.js";
-import CollectionPreview from "../../components/collection-preview/collection-preview.component.jsx";
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      collections: SHOP_DATA
-    };
-  }
-  render() {
-    const { collections } = this.state;
-    return (
+const ShopPage = ({ collections } ) => (
       <div className="shop-page">
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
+       <CollectionsOverview />
       </div>
     );
-  }
-}
+
 
 export default ShopPage;
