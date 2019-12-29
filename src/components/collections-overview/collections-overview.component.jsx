85a74e7a0b68688524cca185b057f6a57bd632ca
Amrendra Kumar <amarmca51@gmail.com>
2019-12-29 15:45:45 +0530

    moved our data to reducer created crosponding selectors and data

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -5,7 +5,7 @@ import './collections-overview.styles.scss';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import CollectionPreview from '../collection-preview/collection-preview.component';
-import { selectCollections } from '../../redux/shop/shop.selectors';
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
 
 const CollectionsOverview =( {collections}) =>{
     return (
@@ -18,7 +18,8 @@ const CollectionsOverview =( {collections}) =>{
 }
 
 const mapStateToProps = createStructuredSelector({
-    collections : selectCollections
+    //collections : selectCollections
+    collections : selectCollectionsForPreview
  
   }) 
 
